dc49dc56869da231d69f43080d17432afaefa563
Ashwini Garad <ashwinigarad1041@gmail.com>
2024-08-06 09:03:38 +0530

    Comparing length of two line

--- LineComparision.java
+++ LineComparision.java
@@ -14,7 +14,8 @@ public class LineComparision {
 
         double length1 = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2 - y1),2));
 
-       System.out.printf("Length of line is %.2f%n" ,length1);
+       System.out.printf("Length of First line is %.2f%n" ,length1);
+       System.out.println();
 
        System.out.print("Enter Points X3 and Y3 : ");
         int x3 = sc.nextInt();
@@ -23,21 +24,37 @@ public class LineComparision {
         System.out.print("Enter Points X4 and Y4 : ");
         int x4 = sc.nextInt();
         int y4 = sc.nextInt();
+        sc.close();
 
         double length2 = Math.sqrt(Math.pow((x4-x3),2) + Math.pow((y4 - y3),2));
-        System.out.printf("Length of line is %.2f%n" ,length2);
+        System.out.printf("Length of Second line is %.2f%n" ,length2);
 
         String line1Length = String.valueOf(length1);
         String line2Length = String.valueOf(length2);
 
-
+        
         if(line1Length.equals(line2Length)){
            System.out.println("Two lines are equal. ");
         }else{
             System.out.println("Two lines are not equal. ");
 
         }
+        System.out.println();
+        System.out.println("Comparision of length : ");
+        int comparison = line1Length.compareTo(line2Length);
+
+        if(comparison > 0){
+            System.out.println("Length of First line is grater than second line ");
+        }else if(comparison < 0){
+            System.out.println("Length of Second line is grater than First line ");
+
+        }else{
+            System.out.println("Length of both line is eqaul ");
+        }
+
+
+        }
      
 
-    }
-}
+ }
+
